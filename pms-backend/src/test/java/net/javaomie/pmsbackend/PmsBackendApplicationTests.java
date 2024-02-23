63694c7bc6049cd01d56589ce3a02ae2014c7d50
Omielee <omielee@hotmail.com>
2024-02-23 17:21:05 +0000

    Backend Framework init

---
+++ pms-backend/src/test/java/net/javaomie/pmsbackend/PmsBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package net.javaomie.pmsbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PmsBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
